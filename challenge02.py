a55c814aa7e9e9654587ab1f750087b9ab64c88c
Will Crawford <will@metawhimsy.com>
2010-08-29 21:37:44 -0700

    I fixed some bugs in challenge 2's solution. #more #spoiler
    The solution mentions string.maketrans()
    What the heck is that?

--- challenge02.py
+++ challenge02.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-challene2.py
+challenge2.py
+http://www.pythonchallenge.com/pc/def/map.html
 
 Created by whimsy on 2010-08-29.
 Copyright (c) 2010 Will Crawford. All rights reserved.
 
-The answer is ocr.html, I think.
+I think the answer is this:
+http://www.pythonchallenge.com/pc/def/ocr.html
 """
 
 import sys
@@ -16,7 +18,7 @@ import os
 def main():
 	a = list("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
 	for i in range(0,len(a)):
-		if a[i] != (' ' or '(' or ')' or '.' or "'"):
+		if a[i] not in [" ", "(", ")", ".", "'"]:
 			a[i] = chr(ord(a[i]) + 2)
 			if ord(a[i]) > ord('z'):
 				a[i] = chr(ord(a[i]) - 26)

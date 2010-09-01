8f0c3361b7572473ab0a610138ece10d3c1b2194
Will Crawford <will@metawhimsy.com>
2010-08-31 17:01:36 -0700

    Started challenge 2

---
+++ challenge02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+challenge01.py
+
+Created by yt on 2010-08-31.
+Copyright (c) 2010 . All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+

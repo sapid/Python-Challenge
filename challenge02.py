f4e8ac3768ba11199793267094bc45e212ef9308
Will Crawford <will@metawhimsy.com>
2010-08-31 17:04:14 -0700

    Solved challenge 2

--- challenge02.py
+++ challenge02.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-challenge01.py
+challenge02.py
+http://www.pythonchallenge.com/pc/def/ocr.html
 
-Created by yt on 2010-08-31.
-Copyright (c) 2010 . All rights reserved.
+Created by whimsy on 2010-08-31.
+Copyright (c) 2010 Will Crawford. All rights reserved.
+
+This file references ocr.txt; ensure you grabbed it from the repo.
 """
 
 import sys
@@ -12,7 +15,7 @@ import os
 
 
 def main():
-	pass
+	return 0
 
 
 if __name__ == '__main__':

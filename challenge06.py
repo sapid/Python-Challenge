c4209d1c5632a3aa1c3597cbab1b2dbb5e2efafd
Will Crawford <will@metawhimsy.com>
2010-11-10 13:33:24 -0800

    Started challenge 06

---
+++ challenge06.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+challenge06.py
+http://www.pythonchallenge.com/pc/def/channel.html
+zip:
+http://www.pythonchallenge.com/pc/def/channel.zip
+
+Started by whimsy on 2010-11-10.
+Completed on... [incomplete]
+Copyright (c) 2010 Will Crawford. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	print "Thus begins challenge 6!"
+	return 0
+
+if __name__ == '__main__': main()

e5eccdcedf30668732bb2d19b01b7b7105a873dd
Will Crawford <will@metawhimsy.com>
2011-06-25 11:49:33 -0700

    Started challenge 7

---
+++ challenge07.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+challenge06.py
+http://www.pythonchallenge.com/pc/def/oxygen.html
+image:
+http://www.pythonchallenge.com/pc/def/oxygen.png
+
+Started by whimsy on 2011-6-25.
+Completed on... [incomplete]
+Copyright (c) 2011 Will Crawford. All rights reserved.
+"""
+
+import sys
+import os
+
+def main():
+      return 0
+
+if __name__ == '__main__': main()
+

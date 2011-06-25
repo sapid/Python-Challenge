2f8ca5a0076859cf37012ba88db8bf113d6afe61
Will Crawford <will@metawhimsy.com>
2011-06-25 11:34:49 -0700

    Traversal of the text files works now... but no solution has yet been found.

--- challenge06.py
+++ challenge06.py
@@ -13,14 +13,47 @@ Copyright (c) 2010 Will Crawford. All rights reserved.
 
 import sys
 import os
+import re
+import zipfile
 
 
 def main():
-	print "Thus begins challenge 6!"
-	print "The provided hint is:\n"
-	print "welcome to my zipped list"
-	print "start from 90052"
-	print "answer is inside the zip"
-	return 0
+   print "Thus begins challenge 6!"
+   print "The provided hint is:\n"
+   print "welcome to my zipped list"
+   print "start from 90052"
+   print "answer is inside the zip"
+   print "\n"
+   
+   the_zip = None
+   print "Trying ./channel.zip"
+   try:
+      the_zip = zipfile.ZipFile('channel.zip', 'r')
+   except (IOError, RuntimeError):
+      print "Trying ./challenge06.zip"
+      try:
+         the_zip = zipfile.ZipFile('challenge06.zip', 'r')
+      except (IOError, RuntimeError):
+            print "Couldn't find zipfile."
+            return 1
+
+   print "File found. \n\nBeginning scan."
+   pattern = re.compile(r'(\d+)')
+   next_file = '90052'
+   next_file = next_file + '.txt'
+   while next_file:
+      text = the_zip.read(next_file)
+      next_file = None
+      print text
+      try:
+         next_file = (pattern.search(text)).group(0)
+         next_file = next_file + '.txt'
+      except TypeError:
+         return 1
+      except AttributeError:
+         break
+      text = None
+
+   return 0
 
 if __name__ == '__main__': main()

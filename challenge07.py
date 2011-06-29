5510f9bc81ec9388d5d1b666be8e8e1c607b642f
Will Crawford <will@metawhimsy.com>
2011-06-29 00:07:09 -0700

    Challenge 7 solved.

--- challenge07.py
+++ challenge07.py
@@ -13,9 +13,32 @@ Copyright (c) 2011 Will Crawford. All rights reserved.
 
 import sys
 import os
+import Image
 
 def main():
-      return 0
+   try:
+      im = Image.open('challenge07.png')
+   except (IOError):
+      try:
+         im = Image.open('oxygen.png')
+      except:
+         print "Couldn't find image."
+         return 1
+   print "File found. \nBeginning processing...\n"
+   print "Size = " + str(im.size) + "px"
+   pix = im.load()
+   decoded = ''
+   for i in range(87):
+      j = i*7
+      decoded = decoded + chr(pix[j, 43][1])
+   print decoded
+   print "\nHardcoded answer: "
+   ans = [ 105, 110, 116, 101, 103, 114, 105, 116, 121 ]
+   answer = ''
+   for i in ans:
+      answer = answer + chr(i)
+   print answer
+   return 0
 
 if __name__ == '__main__': main()
 
